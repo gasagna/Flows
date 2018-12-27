5ed67666b83bab04728770ed7fa7504330a20079
Davide Lasagna <lasagnadavide@gmail.com>
2018-12-27 14:26:21 +0100

    test speed of coupled arithmetic

--- test/src/test_bench.cpp
+++ test/src/test_bench.cpp
@@ -1,8 +1,8 @@
 #include "catch.hpp"
-#include <Flows.hpp>
+
+#include "Flows.hpp"
+
 #include <valarray>
-#include <iostream>
-#include <tuple>
 #include <cmath>
 #include <chrono>
 
@@ -31,21 +31,22 @@ TEST_CASE("Testing Bench", "[bench]") {
 
 
     auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i != 1000; i++) {
+    for (int i = 0; i != 10000; i++) {
         c  = 2*a  + 3*b  - a*2  + b/1.5;
     }
     auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+    std::chrono::duration<double> elapsed_1 = end - start;
+    
 
     start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i != 1000; i++) {
+    for (int i = 0; i != 10000; i++) {
         x4 = 2*x1 + 3*x2 - x1*2 + x2/1.5;
         y4 = 2*y1 + 3*y2 - y1*2 + y2/1.5;
         q4 = 2*q1 + 3*q2 - q1*2 + q2/1.5;
     }
     end = std::chrono::high_resolution_clock::now();
-    elapsed = end - start;
-    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+    std::chrono::duration<double> elapsed_2 = end - start;
 
+    // difference in time must be < than 5%, mostly due to noise
+    REQUIRE( std::fabs(elapsed_1.count() - elapsed_2.count())/elapsed_1.count() < 0.05);
 }
